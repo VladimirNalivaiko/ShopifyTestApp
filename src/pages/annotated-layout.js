7f30af2718a9bc1d12195030b99a14774caa519b
Vladimir Nalivaiko <nalivaikovova@gmail.com>
2019-09-04 13:57:03 +0300

    added navigation links

--- src/pages/annotated-layout.js
+++ src/pages/annotated-layout.js
@@ -7,22 +7,27 @@ import {
   Page,
   Stack,
   TextField,
+  SettingToggle,
+  TextStyle
 } from '@shopify/polaris';
 
 class AnnotatedLayout extends React.Component {
   state = {
     discount: '10%',
+    enabled: false
   };
 
   render() {
-    const { discount } = this.state;
+    const { discount, enabled } = this.state;
+    const contentStatus = enabled ? 'Disable' : 'Enable';
+    const textStatus = enabled ? 'enabled' : 'disabled';
 
     return (
       <Page>
         <Layout>
           <Layout.AnnotatedSection
             title="Default discount"
-            description="Add a product to Sample App, it will automatically be discounted."
+            description="Add a product to Sample App, it will automatically be discounted." 
           >
             <Card sectioned>
               <Form onSubmit={this.handleSubmit}>
@@ -42,6 +47,21 @@ class AnnotatedLayout extends React.Component {
               </Form>
             </Card>
           </Layout.AnnotatedSection>
+          <Layout.AnnotatedSection
+            title="Price updates"
+            description="Temporarily disable all Sample App price updates"
+          >
+            <SettingToggle
+              action={{
+                content: contentStatus,
+                onAction: this.handleToggle,
+              }}
+              enabled={enabled}
+            >
+              This setting is{' '}
+              <TextStyle variation="strong">{textStatus}</TextStyle>.
+            </SettingToggle>
+          </Layout.AnnotatedSection>
         </Layout>
       </Page>
     );
@@ -56,6 +76,12 @@ class AnnotatedLayout extends React.Component {
 
   handleChange = (field) => {
     return (value) => this.setState({ [field]: value });
+  };
+
+  handleToggle = () => {
+    this.setState(({ enabled }) => {
+      return { enabled: !enabled };
+    });
   };
 }
 
