bea111253704f8f2381fddcb1744fb2e410aa550
Vladimir Nalivaiko <nalivaikovova@gmail.com>
2019-09-04 21:10:47 +0300

    added resource picker

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,26 @@
 import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/styles.css';
+import Cookies from 'js-cookie';
+
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
     return (
       <React.Fragment>
         <Head>
           <title>Sample App</title>
           <meta charSet="utf-8" />
         </Head>
-        <AppProvider>
-          <Component {...pageProps} />
-        </AppProvider>
+        <Provider config={config}>
+          <AppProvider>
+            <Component {...pageProps} />
+          </AppProvider>
+        </Provider>
       </React.Fragment>
     );
   }
